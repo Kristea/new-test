f13c318f2a8f35385026836064ffb3b0af5f50c6
andyxmas <andy.christmas@gmail.com>
2020-06-22 13:50:25 +0100

    we can search blogs...!?!?

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,6 +4,8 @@
       <div class="container">
 
         <h1 class="title is-1">blogs</h1>
+        <input v-model="searchParam" @keyup="searchBlogs">
+        <button @click="searchBlogs">Search</button>
           <ul>
             <li v-for="item in blogPosts" :key="item.date">
               <nuxt-link :to="item.slug" append>
@@ -28,7 +30,15 @@ export default {
   },
   data () {
     return {
-      blogPosts: []
+      blogPosts: [],
+      searchParam: ''
+    }
+  },
+  methods: {
+    async searchBlogs () {
+      let search = this.$content('blog')
+      search = this.searchParam != '' ? search.where({ title: this.searchParam }) : search
+      this.blogPosts = await search.fetch()
     }
   },
   async fetch() {
