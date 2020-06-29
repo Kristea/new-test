cb575710e95f802d03e06a7751390c044c726205
andyxmas <andy.christmas@gmail.com>
2020-06-29 10:19:50 +0100

    components work!

---
+++ pages/slices.vue
@@ -0,0 +1,66 @@
+<template>
+  <page>
+    <template v-slot:main>
+      <div>
+        yo
+        {{ pageContent}}
+        <p v-for="slice in pageContent.slices" :key="slice.template">{{ slice.template}}</p>
+        <component v-for="slice in pageContent.slices" :key="slice._uid" :is="slice.template"></component>
+        <!-- :key="story.slices._uid"  -->
+      </div>
+    </template>
+    <!-- <template v-slot:main>
+      <div class="container">
+        <h1 class="title is-1">{{ pageContent.title }}</h1>
+        <Slideshow 
+          :items="items"
+        />
+        <TextAndImage 
+          :headline="content.headline"
+          :content="content.content"
+          :cta="content.cta"
+          :image="content.image"
+        />
+      </div>
+    </template> -->
+
+  </page>
+</template>
+
+
+
+
+<script>
+import page from '~/components/Page.vue'
+import showcase from '~/components/Slideshow'
+import TextAndImage from '~/components/TextAndImage'
+
+export default {
+  components: {
+    page,
+    showcase,
+    TextAndImage
+  },
+  head() {
+    return {
+      title: this.pageContent.title + ' - ' + 'nrml',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: 'My custom description' }
+      ]
+    }
+  },
+  data() {
+    return {
+      pageContent: {}
+    }
+  },
+  async asyncData({ params, payload }) {
+    let pageContent = await require(`~/assets/content/pages/Slices.json`)
+    console.log(pageContent)
+    return {
+      pageContent
+    }
+  },
+}
+</script>
