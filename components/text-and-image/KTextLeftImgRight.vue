0221f4d3ce741c2c7091f067edbdfd7cc45e49db
Emily Vinsen <emilyv@madeagency.co.uk>
2020-08-13 13:50:57 +0100

    changed slice to pass in props for full img and contained, dont need separate componet for it

---
+++ components/text-and-image/KTextLeftImgRight.vue
@@ -0,0 +1,79 @@
+<template>
+    <div :class="{'k-text-left-img-right': true, container: Container}">
+        <div :class="{image: true, 'img-full': FullImg}" :style="imgStyle">
+            <img src="https://source.unsplash.com/ylvK73Y4G7o" alt="Test"/>
+        </div>
+        <div class="text" :style="textStyle">
+            <h3>Title here</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+            <n-link to="/">Call to action</n-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    // Image covers the size of the image container on the left, text is on the right.
+    // can override the padding/margin of the image box and the text box with css
+    // can pass in text width and img-width for not 50/50 slices
+
+    export default {
+        props: {
+            TextWidth: {
+                type: Number,
+                default: 50
+            },
+            ImgWidth: {
+                type: Number,
+                default: 50
+            },
+            Container: Boolean,
+            FullImg: Boolean
+        },
+        computed: {
+            imgStyle() {
+                return 'flex-basis: ' + this.ImgWidth + '%';
+            },
+            textStyle() {
+                return 'flex-basis: ' + this.TextWidth + '%';
+            }
+        }
+
+    }
+</script>
+
+<style scoped lang="scss">
+    .k-text-left-img-right {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 100vh;
+
+        .image img {
+            width: 100%;
+            height: auto;
+        }
+
+        .img-full {
+            height: 100%;
+            img {
+                height: 100%;
+                width: 100%;
+                object-fit: cover;
+            }
+        }
+
+        @media (max-width: 700px) {
+            flex-wrap: wrap;
+            .image, .text {
+                flex-basis: 100% !important;
+                max-width: 100% !important;
+            }
+        }
+
+        @media (min-width: 700px) {
+            .text {
+                padding-left: 30px;
+            }
+        }
+    }
+</style>
--- pages/text-and-image.vue
+++ pages/text-and-image.vue
@@ -2,19 +2,32 @@
     <div>
         <h1>Text and Image Slices</h1>
 
-        <h2>K Contained Image Full Left, Text Right</h2>
-        <KContainedImgFullLeftTextRight />
+        <h2>Standard text left image right (no props) - should image have max-height ??</h2>
+        <KTextLeftImgRight />
 
-        <h2>K Contained Text Left, Image Full Right</h2>
-        <KContainedTextLeftImgFullRight :TextWidth="33" :ImgWidth="66" />
+        <h2>Full image text left image right</h2>
+        <KTextLeftImgRight :full-img="true" />
+
+        <h2>Contained text left image right</h2>
+        <KTextLeftImgRight :container="true" />
+
+        <h2>Contained, full image, two thirds img, one third text</h2>
+        <KTextLeftImgRight :container="true" :full-img="true" :img-width="66" :text-width="33" />
+
+        <!--<h2>K Contained Image Full Left, Text Right</h2>-->
+        <!--<KContainedImgFullLeftTextRight />-->
+
+        <!--<h2>K Contained Text Left, Image Full Right</h2>-->
+        <!--<KContainedTextLeftImgFullRight :TextWidth="33" :ImgWidth="66" />-->
     </div>
 </template>
 
 <script>
     import KContainedImgFullLeftTextRight from "../components/text-and-image/KContainedImgFullLeftTextRight";
     import KContainedTextLeftImgFullRight from "../components/text-and-image/KContainedTextLeftImgFullRight";
+    import KTextLeftImgRight from "../components/text-and-image/KTextLeftImgRight";
 
     export default {
-        components: {KContainedImgFullLeftTextRight, KContainedTextLeftImgFullRight}
+        components: {KContainedImgFullLeftTextRight, KContainedTextLeftImgFullRight, KTextLeftImgRight}
     }
 </script>
