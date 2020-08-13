78cf190746e914e6ddde476d67058f1c1c338e13
Emily Vinsen <emilyv@madeagency.co.uk>
2020-08-13 14:43:13 +0100

    delete old separate full img slices

--- pages/text-and-image.vue
+++ pages/text-and-image.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>Text and Image Slices</h1>
 
-        <h2>Standard text left image right (no props) - should image have max-height ??</h2>
+        <h2>Standard text left image right (no props) - should image have max-height when not full?? Or should it have to match height of text??</h2>
         <KTextLeftImgRight />
 
         <h2>Full image, image left, text right</h2>
@@ -13,22 +13,14 @@
 
         <h2>Contained, full image, two thirds img, one third text</h2>
         <KTextLeftImgRight :Container="true" :full-img="true" :img-width="66" :text-width="33" />
-
-        <!--<h2>K Contained Image Full Left, Text Right</h2>-->
-        <!--<KContainedImgFullLeftTextRight />-->
-
-        <!--<h2>K Contained Text Left, Image Full Right</h2>-->
-        <!--<KContainedTextLeftImgFullRight :TextWidth="33" :ImgWidth="66" />-->
     </div>
 </template>
 
 <script>
-    import KContainedImgFullLeftTextRight from "../components/text-and-image/KContainedImgFullLeftTextRight";
-    import KContainedTextLeftImgFullRight from "../components/text-and-image/KContainedTextLeftImgFullRight";
     import KTextLeftImgRight from "../components/text-and-image/KTextLeftImgRight";
     import KImgLeftTextRight from "../components/text-and-image/KImgLeftTextRight";
 
     export default {
-        components: {KContainedImgFullLeftTextRight, KContainedTextLeftImgFullRight, KTextLeftImgRight, KImgLeftTextRight}
+        components: {KTextLeftImgRight, KImgLeftTextRight}
     }
 </script>
